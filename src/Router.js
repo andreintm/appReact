8a1e3c06a675cf307750e05fcc271f5889296f2a
Andrei <andrei.ntm@gmail.com>
2017-11-14 23:45:53 +0200

    employee list

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {Router, Scene, Stack} from 'react-native-router-flux';
+import {Router, Scene, Actions} from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
-        <Router>
-            <Stack>
+        <Router sceneStyle={{ paddingTop: 65 }}>
+            <Scene key="auth">
                 <Scene key='login' component={LoginForm} title="Please Login"/>
-                <Scene key='employeeList' component={EmployeeList} title="Employees"/>
-            </Stack>
+            </Scene>
+            <Scene key="main">
+                <Scene
+                    onRight={() => Actions.employeeCreate()}
+                    rightTitle="Add"
+                    key='employeeList'
+                    component={EmployeeList}
+                    title="Employees"
+                    initial/>
+                <Scene key='employeeCreate' component={EmployeeCreate} title='Create Employee'/>
+            </Scene>
         </Router>
     );
 };
